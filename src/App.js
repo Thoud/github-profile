83c396e7cd3bbbd6f15f02c7d3539870f6c79fe2
Robert Miron <73788820+Thoud@users.noreply.github.com>
2021-02-23 12:18:55 +0100

    Add functionality to retrieve profile informations

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
+import { gql, useQuery } from '@apollo/client';
+
+const profileQuery = gql`
+  query {
+    viewer {
+      login
+      avatarUrl
+      company
+      repositories(first: 5) {
+        nodes {
+          name
+          createdAt
+          defaultBranchRef {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
 export default function App() {
-  return <div />;
+  const { loading, error, data } = useQuery(profileQuery);
+  if (loading) return 'Loading ...';
+  if (error) return 'Something went wrong!';
+
+  return (
+    <>
+      <img src={data.viewer.avatarUrl} height="200" alt="Profile" />
+      <p>{data.viewer.company}</p>
+      <ul>
+        {data.viewer.repositories.nodes.map((repo) => {
+          return <li key={repo.id}>{repo.name}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    // Please insert your personal access token from your GitHub profile below
+    authorization: 'Bearer <insert token here>',
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
